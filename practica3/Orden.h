a245d2464dfd797675f1a2be9752730e306e2c88
El Monje Pastillero <48119585+politesax09@users.noreply.github.com>
2019-04-29 15:50:31 +0200

    Add files via upload

--- practica3/Orden.h
+++ practica3/Orden.h
@@ -1,1 +1,23 @@
 #include "ListaContigua.h"
+
+#define ASC0	// de menor a mayor
+#define DESC1	// de mayor a menor
+
+class Orden
+{
+public:
+	Orden();
+	
+	bool esOrdenada(ListaContigua &lista, int direccion);
+
+	//Metodos de ordenacion
+	void insercion(ListaContigua &lista, int direccion);
+	void seleccion(ListaContigua &lista, int direccion);
+	void burbuja(ListaContigua &lista, int direccion);
+	void QSort(ListaContigua &lista, int direccion);
+	void MSort(ListaContigua &lista, int direccion);
+	void rango(ListaContigua &lista, int direccion);
+
+	~Orden();
+	
+};
