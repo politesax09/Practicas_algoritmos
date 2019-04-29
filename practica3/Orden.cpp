e6756b9ce53c4e0134a8bd8e4cabf339c6320cc7
El Monje Pastillero <48119585+politesax09@users.noreply.github.com>
2019-04-29 16:17:22 +0200

    Add files via upload

--- practica3/Orden.cpp
+++ practica3/Orden.cpp
@@ -1,1 +1,20 @@
 #include "Orden.h"
+
+bool Orden::esOrdenada(ListaContigua *lista, int direccion){
+	int i = 0;
+
+	if (direccion == ASC)
+	{
+		while (lista->getValor(i - 1) < lista->getValor(i) && i < lista->getN())
+		if (i == lista->getN() - 1)
+			return true;
+		return false;
+	}
+	else
+	{
+		while (lista->getValor(i - 1) > lista->getValor(i) && i < lista->getN())
+		if (i == lista->getN() - 1)
+			return true;
+		return false;				
+	}
+}
