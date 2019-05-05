9ebfa97d3f43e6e58e0d2017941f48870f92c92d
El Monje Pastillero <48119585+politesax09@users.noreply.github.com>
2019-05-05 02:20:31 +0200

    Add files via upload

--- practica3/Orden.cpp
+++ practica3/Orden.cpp
@@ -18,3 +18,50 @@ bool Orden::esOrdenada(ListaContigua *lista, int direccion){
 		return false;				
 	}
 }
+
+void Orden::insercion(ListaContigua *lista, int direccion){
+	int temp = 0, i, j, k;
+
+	for (i = 0; i < lista->getN(); ++i)
+	{
+		temp = lista->getValor(i);
+
+		for (j = i - 1, k = i - 1; j >= 0; --j)
+		{
+			if (lista->getValor(i) < lista->getValor(j))
+			{
+				for (; k - 1 > j; --k)
+					lista->setValor(k, lista->getValor(k - 1));
+				lista->setValor(j, temp);	
+			}
+		}
+	}
+}
+
+void Orden::seleccion(ListaContigua *lista, int direccion){
+	int i, j, menor, posMenor;
+	for (i = 0; i < lista->getN(); ++i)
+	{
+		menor = lista->getValor(i);
+		posMenor = 0;
+
+		//Buscar el menor elemento de la semi-lista y guaradarlo junto a su posicion
+		for (j = i; j < lista->getN(); ++j)
+		{
+			if (lista->getValor(j) < menor)
+			{
+				menor = lista->getValor(j);
+				posMenor = j;
+			}
+		}
+		
+		//Intercambiar el elemento "i" con el "j"
+		//Si posMenor == 0, significa que el "i" es el menor elemento en ese momento
+		if (posMenor != 0)
+		{
+			lista->setValor(posMenor, lista->getValor(i));
+			lista->setValor(i, menor);		
+		}
+
+	}
+}
--- practica3/Orden.h
+++ practica3/Orden.h
@@ -7,16 +7,16 @@ class Orden
 {
 public:
 	Orden();
-	
+
 	bool esOrdenada(ListaContigua *lista, int direccion);
 
 	//Metodos de ordenacion
-	void insercion(ListaContigua &lista, int direccion);
-	void seleccion(ListaContigua &lista, int direccion);
-	void burbuja(ListaContigua &lista, int direccion);
-	void QSort(ListaContigua &lista, int direccion);
-	void MSort(ListaContigua &lista, int direccion);
-	void rango(ListaContigua &lista, int direccion);
+	void insercion(ListaContigua *lista, int direccion);
+	void seleccion(ListaContigua *lista, int direccion);
+	void burbuja(ListaContigua *lista, int direccion);
+	void QSort(ListaContigua *lista, int direccion);
+	void MSort(ListaContigua *lista, int direccion);
+	void rango(ListaContigua *lista, int direccion);
 
 	~Orden();
 	
